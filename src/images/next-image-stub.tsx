83d1fc80f16a41389c6f279dbb6121bd160829d6
Ryan Clements <ryanclementshax@gmail.com>
2022-05-23 18:09:10 -0400

    fix(next/image): handle new next/image export format
    fixes #72

--- src/images/next-image-stub.tsx
+++ src/images/next-image-stub.tsx
@@ -1,15 +1,15 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as _NextImage from 'next/image'
 import { ImageProps } from 'next/image'
 import semver from 'semver'
 
 // next v9 (doesn't have next/image)
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 if (semver.gt(process.env.__NEXT_VERSION!, '9.0.0')) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const NextImage = require('next/image') as typeof _NextImage
+
   const OriginalNextImage = NextImage.default
 
-  // eslint-disable-next-line no-import-assign
   Object.defineProperty(NextImage, 'default', {
     configurable: true,
     value: (props: ImageProps) =>
@@ -19,4 +19,12 @@ if (semver.gt(process.env.__NEXT_VERSION!, '9.0.0')) {
         <OriginalNextImage {...props} unoptimized />
       )
   })
+
+  // https://github.com/vercel/next.js/issues/36417#issuecomment-1117360509
+  if (semver.gte(process.env.__NEXT_VERSION!, '12.1.5')) {
+    Object.defineProperty(NextImage, '__esModule', {
+      configurable: true,
+      value: true
+    })
+  }
 }
